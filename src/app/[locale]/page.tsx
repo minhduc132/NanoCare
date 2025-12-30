81b060ff535d2c306834c4049b2da49ff0bb0d36
MinhDucNguyen <nguyenminhduc01032001@gmail.com>
2025-12-30 09:13:00 +0700

    update code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,7 +12,7 @@ import Footer from '../../components/Footer';
 
 export default function Home() {
     return (
-        <main className="min-h-screen">
+        <main className="min-h-screen font-[TWFucmwZSFeHRyYUxpZhLnRZg]">
             <Hero />
             <About />
             <Offerings />
